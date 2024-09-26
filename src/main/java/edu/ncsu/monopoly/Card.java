66425884aad31abe7775871599cd114d8b644ef4
ikerg <ikerg@LAPTOP-9A2MV3ED>
2024-09-26 16:41:47 +0200

    cambios

--- src/main/java/edu/ncsu/monopoly/Card.java
+++ src/main/java/edu/ncsu/monopoly/Card.java
@@ -1,5 +1,5 @@
 package edu.ncsu.monopoly;
-//I have added a comment MODIFICADO 26/09
+//I have added a comment MODIFICADO 26/09 OTRO CAMBIO
 public abstract class Card {
 
     public static final int TYPE_CHANCE = 1;
